d7b2c13e486500bd262b8de111739761426ada3c
caudencia wanyama <conny@students.uonbi.ac.ke>
2022-06-15 01:59:47 +0300

    commit tree ee638f3d3db82bab1ddfbb33b62972f62d6f80ab

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]
+let totalBatteries= batteryBatches.reduce(function(total,batteries){
+    return total +batteries
+});
 
 // Code your solution here
+/*
+const couponLocations = [
+    { room: 'Living room', amount: 5 },
+    { room: 'Kitchen', amount: 2 },
+    { room: 'Bathroom', amount: 1 },
+    { room: 'Master bedroom', amount: 7 }
+  ];
+  
+function ourReduce(arr, reducer, init) {
+    let accum = init;
+    for (const element of arr) {
+        accum = reducer(accum, element);
+    };
+    return accum;
+}
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+console.log(ourReduce(couponLocations, couponCounter, 0)); // LOG: 15
+console.log(ourReduce(couponLocations, couponCounter, 3)); // LOG: 18
+
+const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){ return element * 2 + accumulator}, 7)
+// => 12
+console.log(doubledAndSummed)*/
+
